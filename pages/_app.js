424fc997b0ed7de08ea084e26e03338c9471f890
Matt <matt.heinke@gmail.com>
2022-02-02 09:53:38 -0800

    update starterkit

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,16 @@
-import Head from 'next/head';
-import Script from 'next/script';
-
-import 'tailwindcss/tailwind.css';
+import '../src/styles/global.css';
+import '../src/styles/variables.css';
 import '@ionic/react/css/core.css';
-import '@ionic/react/css/padding.css';
+import '@ionic/react/css/display.css';
+import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/padding.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import 'tailwindcss/tailwind.css';
 
-import '../styles/global.css';
-import '../styles/variables.css';
+import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
